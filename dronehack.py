ca6a44fe7d39594500df54d592445285384ee426
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-07-04 17:00:47 -0300

    dronehack.py

--- dronehack.py
+++ dronehack.py
@@ -5,9 +5,8 @@ import os
 from threading import Thread
 from time import sleep
 
-
 def ascii():
-   print("""                                      .:-======-:.                                                                    
+    print("""                                      .:-======-:.                                                                    
                                              .=*##+=-:....:-=+*#*-                                                                
                                            .*%=.                .=%*.                                                             
                                           :@+         =+++:        =@:                                                            
@@ -33,9 +32,6 @@ def ascii():
                                                                     :@@#-                                                         
                                                                      :.                                       """)
 
-
-ascii()
-
 # Verifica se a biblioteca djitellopy está instalada
 try:
     importlib.import_module('djitellopy')
@@ -51,7 +47,7 @@ except ImportError:
         exit()
 
 # Importa a biblioteca djitellopy
-from DJITelloPy.djitellopy import Tello
+from djitellopy import Tello
 
 # Obtém o endereço IP da interface de rede
 def get_local_ip():
@@ -129,6 +125,15 @@ def video_handler(event, sender, data):
 def send_command(command):
     drone.send_command(command)
 
+# Desconectar o drone
+def disconnect_drone(drone):
+    drone.disconnect()
+
+# Desconectar todos os drones
+def disconnect_all_drones(drones):
+    for drone in drones:
+        drone.disconnect()
+
 # Conectar ao drone
 drone_ip = choose_drone_ip()
 drone_port = get_drone_port()
@@ -142,58 +147,73 @@ drone.subscribe(drone.EVENT_VIDEO_FRAME, video_handler)
 # Iniciar stream de vídeo
 drone.streamon()
 
-# Lista de comandos disponíveis
-command_list = [
-    "takeoff",
-    "land",
-    "up {distance}",
-    "down {distance}",
-    "left {distance}",
-    "right {distance}",
-    "forward {distance}",
-    "back {distance}",
-    "flip {direction}",
-    "cw {angle}",
-    "ccw {angle}",
-    "battery?",
-    "capture",
-    "record {time}"
-]
+# Menu de controle ou derrubada
+while True:
+    ascii()
+    print("\n1. Controlar o drone")
+    print("2. Derrubar o drone")
+    print("0. Sair\n")
 
-# Imprimir a lista de comandos disponíveis
-print("Comandos disponíveis:")
-for i, command in enumerate(command_list, 1):
-    print(f"{i}. {command}")
+    choice = input("Digite o número da opção desejada: ")
 
-# Função para processar os comandos digitados pelo usuário
-def process_user_commands():
-    while True:
-        user_input = input("Digite o número do comando para o drone (ou 'sair' para encerrar): ")
-        if user_input.lower() == "sair":
-            break
-        try:
-            command_index = int(user_input) - 1
-            if 0 <= command_index < len(command_list):
-                command = command_list[command_index]
-                if "{distance}" in command or "{angle}" in command:
-                    value = input("Digite a distância ou ângulo: ")
-                    command = command.replace("{distance}", value).replace("{angle}", value)
-                send_command(command)
-            else:
-                print("Comando inválido. Tente novamente.")
-        except ValueError:
-            print("Entrada inválida. Digite um número ou 'sair' para encerrar.")
+    if choice == "1":
+        # Lista de comandos disponíveis
+        command_list = [
+            "takeoff",
+            "land",
+            "up {distance}",
+            "down {distance}",
+            "left {distance}",
+            "right {distance}",
+            "forward {distance}",
+            "back {distance}",
+            "flip {direction}",
+            "cw {angle}",
+            "ccw {angle}",
+            "battery?",
+            "capture",
+            "record {time}"
+        ]
 
-# Iniciar o processamento de comandos do usuário em uma thread separada
-thread = Thread(target=process_user_commands)
-thread.start()
+        # Imprimir a lista de comandos disponíveis
+        print("\nComandos disponíveis:")
+        for i, command in enumerate(command_list, 1):
+            print(f"{i}. {command}")
 
-# Aguardar o encerramento da thread de processamento de comandos
-thread.join()
+        # Função para processar os comandos digitados pelo usuário
+        def process_user_commands():
+            while True:
+                user_input = input("\nDigite o número do comando para o drone (ou 'sair' para encerrar): ")
+                if user_input.lower() == "sair":
+                    break
+                try:
+                    command_index = int(user_input) - 1
+                    if 0 <= command_index < len(command_list):
+                        command = command_list[command_index]
+                        if "{distance}" in command or "{angle}" in command:
+                            value = input("Digite a distância ou ângulo: ")
+                            command = command.replace("{distance}", value).replace("{angle}", value)
+                        send_command(command)
+                    else:
+                        print("Comando inválido. Tente novamente.")
+                except ValueError:
+                    print("Entrada inválida. Digite um número ou 'sair' para encerrar.")
 
-# Parar o stream de vídeo
-drone.streamoff()
+        # Iniciar o processamento de comandos do usuário em uma thread separada
+        thread = Thread(target=process_user_commands)
+        thread.start()
 
-# Desconectar do drone
-drone.disconnect()
+        # Aguardar o encerramento da thread de processamento de comandos
+        thread.join()
+        
+    elif choice == "2":
+        disconnect_drone(drone)
+        print("Drone desconectado.")
 
+    elif choice == "0":
+        disconnect_all_drones([drone])
+        print("Programa encerrado.")
+        break
+
+    else:
+        print("Escolha inválida. Tente novamente.")
