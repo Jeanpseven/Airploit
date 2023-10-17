7f726e4d6b21b0fbd95ce051aa1c5c4ffb74cbbf
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-10-16 22:36:17 -0300

    Update dronehack.py

--- dronehack.py
+++ dronehack.py
@@ -38,130 +38,32 @@ def ascii():
                                                                     :@@#-                                                         
                                                                      :.                                       """)
 
-# Instalar as bibliotecas necessárias
-libraries = ["Pillow", "djitellopy", "netifaces", "xvfbwrapper"]
-
-for library in libraries:
-    try:
-        importlib.import_module(library)
-        print(f"A biblioteca {library} já está instalada.")
-    except ImportError:
-        print(f"A biblioteca {library} não foi encontrada. Instalando...")
-        try:
-            subprocess.check_call(["pip", "install", library])
-            print(f"A biblioteca {library} foi instalada com sucesso.")
-        except Exception as e:
-            print(f"Ocorreu um erro durante a instalação da biblioteca {library}:")
-            print(str(e))
-            exit()
-
-# Função para fechar a janela
-def close_window():
-    window.destroy()
-
-# Função para exibir a janela gráfica
-def display_window():
-    # Criar janela
-    window = tk.Tk()
-    window.title("Controle do Drone")
-    window.geometry("800x600")
-
-    # Criar rótulo para exibir a imagem do vídeo
-    label = tk.Label(window)
-    label.pack()
-
-    # Criar botões de controle
-    takeoff_button = tk.Button(window, text="Decolar", command=takeoff)
-    takeoff_button.pack()
-
-    land_button = tk.Button(window, text="Pousar", command=land)
-    land_button.pack()
-
-    forward_button = tk.Button(window, text="Avançar", command=move_forward)
-    forward_button.pack()
-
-    backward_button = tk.Button(window, text="Recuar", command=move_backward)
-    backward_button.pack()
-
-    left_button = tk.Button(window, text="Esquerda", command=move_left)
-    left_button.pack()
-
-    right_button = tk.Button(window, text="Direita", command=move_right)
-    right_button.pack()
-
-    # Iniciar captura de vídeo em uma thread separada
-    video_thread = Thread(target=capture_video)
-    video_thread.daemon = True
-    video_thread.start()
-
-    # Criar botão de fechar
-    button = tk.Button(window, text="Fechar", command=close_window)
-    button.pack()
-
-    # Executar o loop principal da janela
-    window.mainloop()
-
-# Função para capturar o feed de vídeo do drone
-def capture_video():
-    while True:
-        frame = drone.get_frame_read().frame
-        image = Image.fromarray(frame)
-        photo = ImageTk.PhotoImage(image)
-        label.config(image=photo)
-        label.image = photo
-        sleep(0.01)
-
-# Funções para controlar o drone
-def takeoff():
-    drone.takeoff()
-
-def land():
-    drone.land()
-
-def move_forward():
-    drone.move_forward(50)
-
-def move_backward():
-    drone.move_backward(50)
-
-def move_left():
-    drone.move_left(50)
-
-def move_right():
-    drone.move_right(50)
-
-# (Conexão e controle do drone)
-
-# Verifica se a biblioteca djitellopy está instalada
-try:
-    importlib.import_module('djitellopy')
-    print("Biblioteca djitellopy já está instalada.")
-except ImportError:
-    print("Biblioteca djitellopy não encontrada. Baixando do repositório...")
-    try:
-        os.system("git clone https://github.com/damiafuentes/DJITelloPy.git")
-        print("Biblioteca djitellopy baixada com sucesso.")
-    except Exception as e:
-        print("Ocorreu um erro durante o download da biblioteca djitellopy:")
-        print(str(e))
-        exit()
-
-# Importa a biblioteca djitellopy
+import subprocess
+import importlib
+import netifaces
+import socket
 from djitellopy import Tello
+import cv2
+import tkinter as tk
+from PIL import Image, ImageTk
 
-# Obtém o endereço IP da interface de rede
-def get_local_ip():
-    interfaces = netifaces.interfaces()
-    for interface in interfaces:
-        if interface != 'lo':
-            addresses = netifaces.ifaddresses(interface)
-            if netifaces.AF_INET in addresses:
-                ipv4_addresses = addresses[netifaces.AF_INET]
-                if len(ipv4_addresses) > 0:
-                    return ipv4_addresses[0]['addr']
-    return None
+# Instalar as bibliotecas necessárias
+def install_libraries():
+    libraries = ["Pillow", "djitellopy", "netifaces", "opencv-python"]
+    for library in libraries:
+        try:
+            importlib.import_module(library)
+            print(f"A biblioteca {library} já está instalada.")
+        except ImportError:
+            print(f"A biblioteca {library} não foi encontrada. Instalando...")
+            try:
+                subprocess.check_call(["pip", "install", library])
+                print(f"A biblioteca {library} foi instalada com sucesso.")
+            except Exception as e:
+                print(f"Ocorreu um erro durante a instalação da biblioteca {library}:")
+                print(str(e))
 
-# Lista os IPs disponíveis na rede local
+# Função para listar IPs disponíveis na rede local
 def list_available_ips():
     local_ip = get_local_ip()
     if local_ip:
@@ -178,13 +80,13 @@ def list_available_ips():
         return available_ips
     return []
 
-# Lista os IPs disponíveis e permite selecionar o IP do drone
+# Função para escolher um IP de drone da lista enumerada
 def choose_drone_ip():
     ips = list_available_ips()
     if ips:
         print("IPs disponíveis:")
-        for i, ip in enumerate(ips):
-            print(f"{i+1}. {ip}")
+        for i, ip in enumerate(ips, 1):
+            print(f"{i}. {ip}")
 
         while True:
             try:
@@ -199,122 +101,91 @@ def choose_drone_ip():
         print("Nenhum IP de drone encontrado.")
         return None
 
-# Função para obter a porta para conexão
-def get_drone_port():
-    default_port = 8889
-    user_input = input(f"Digite a porta do drone (padrão {default_port}) ou deixe em branco para descoberta automática: ")
-    if user_input == "":
-        return default_port
-    try:
-        port = int(user_input)
-        return port
-    except ValueError:
-        print("Entrada inválida. Usando a porta padrão.")
-        return default_port
-
-# Desconectar o drone
-def disconnect_drone(drone):
-    drone.disconnect()
-
 # Conectar ao drone
-drone_ip = choose_drone_ip()
-drone_port = get_drone_port()
-drone = Tello(drone_ip, drone_port)
-drone.connect()
+def connect_to_drone(drone_ip, drone_port):
+    try:
+        drone = Tello(drone_ip, drone_port)
+        drone.connect()
+        drone.streamon()
+        ascii()
+        print(f"Conectado ao drone com IP {drone_ip} na porta {drone_port}.")
+        return drone
+    except Exception as e:
+        print(f"Ocorreu um erro ao conectar ao drone:")
+        print(str(e))
+        return None
 
-# Iniciar stream de vídeo
-drone.streamon()
+# Função para desconectar o drone
+def disconnect_drone(drone):
+    drone.land()
+    drone.streamoff()
+    drone.end()
+    print("Desconectado do drone.")
 
-# Exibir a janela gráfica em uma thread separada
-window_thread = Thread(target=display_window)
-window_thread.daemon = True
-window_thread.start()
+# Capturar o feed de vídeo do drone
+def capture_video(drone, label):
+    while True:
+        frame = drone.get_frame_read().frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        label.config(image=photo)
+        label.image = photo
 
-# Menu de controle ou derrubada
-while True:
-    ascii()
-    print("\n1. Controlar o drone")
-    print("2. Derrubar um drone")
-    print("3. Derrubar todos os drones")
-    print("0. Sair\n")
+# Função para exibir a interface gráfica
+def display_interface(drone):
+    root = tk.Tk()
+    root.title("Controle do Drone")
+    root.geometry("800x600")
 
-    choice = input("Digite o número da opção desejada: ")
+    label = tk.Label(root)
+    label.pack()
 
-    if choice == "1":
-        # Lista de comandos disponíveis
-        command_list = [
-            "takeoff",
-            "land",
-            "up {distance}",
-            "down {distance}",
-            "left {distance}",
-            "right {distance}",
-            "forward {distance}",
-            "back {distance}",
-            "flip {direction}",
-            "cw {angle}",
-            "ccw {angle}",
-            "battery?",
-            "capture",
-            "record {time}"
-        ]
+    takeoff_button = tk.Button(root, text="Decolar", command=drone.takeoff)
+    takeoff_button.pack()
 
-        # Imprimir a lista de comandos disponíveis
-        print("\nComandos disponíveis:")
-        for i, command in enumerate(command_list, 1):
-            print(f"{i}. {command}")
+    land_button = tk.Button(root, text="Pousar", command=drone.land)
+    land_button.pack()
 
-        # Função para processar os comandos digitados pelo usuário
-        def process_user_commands():
-            while True:
-                user_input = input("\nDigite o número do comando para o drone (ou 'sair' para encerrar): ")
-                if user_input.lower() == "sair":
-                    break
-                try:
-                    command_index = int(user_input) - 1
-                    if 0 <= command_index < len(command_list):
-                        command = command_list[command_index]
-                        if "{distance}" in command or "{angle}" in command:
-                            value = input("Digite a distância ou ângulo: ")
-                            command = command.replace("{distance}", value).replace("{angle}", value)
-                        send_command(command)
-                    else:
-                        print("Comando inválido. Tente novamente.")
-                except ValueError:
-                    print("Entrada inválida. Digite um número ou 'sair' para encerrar.")
+    forward_button = tk.Button(root, text="Avançar", command=lambda: drone.move_forward(20))
+    forward_button.pack()
 
-        # Iniciar o processamento de comandos do usuário em uma thread separada
-        thread = Thread(target=process_user_commands)
-        thread.start()
+    backward_button = tk.Button(root, text="Recuar", command=lambda: drone.move_back(20))
+    backward_button.pack()
 
-        # Aguardar o encerramento da thread de processamento de comandos
-        thread.join()
+    left_button = tk.Button(root, text="Esquerda", command=lambda: drone.move_left(20))
+    left_button.pack()
 
-    elif choice == "2":
-        drone_ip = input("Digite o IP do drone que deseja derrubar: ")
-        drone_port = get_drone_port()
-        drone_to_drop = Tello(drone_ip, drone_port)
-        drone_to_drop.connect()
-        drone_to_drop.land()
-        disconnect_drone(drone_to_drop)
-        print("Drone derrubado.")
+    right_button = tk.Button(root, text="Direita", command=lambda: drone.move_right(20))
+    right_button.pack()
 
-    elif choice == "3":
-        available_ips = list_available_ips()
-        if available_ips:
-            for ip in available_ips:
-                drone_to_drop = Tello(ip, get_drone_port())
-                drone_to_drop.connect()
-                drone_to_drop.land()
-                disconnect_drone(drone_to_drop)
-            print("Todos os drones foram derrubados.")
-        else:
-            print("Nenhum drone encontrado.")
+    cw_button = tk.Button(root, text="Girar Direita", command=lambda: drone.rotate_clockwise(90))
+    cw_button.pack()
 
-    elif choice == "0":
+    ccw_button = tk.Button(root, text="Girar Esquerda", command=lambda: drone.rotate_counter_clockwise(90))
+    ccw_button.pack()
+
+    video_thread = tk.Thread(target=capture_video, args=(drone, label))
+    video_thread.daemon = True
+    video_thread.start()
+
+    root.mainloop()
+
+# Função principal
+def main():
+    install_libraries()
+    drone_ip = choose_drone_ip()
+    drone_port = 8889
+    drone = connect_to_drone(drone_ip, drone_port)
+
+    if drone is not None:
+        display_interface(drone)
         disconnect_drone(drone)
-        print("Programa encerrado.")
-        break
+    else:
+        print("Não foi possível conectar ao drone. Verifique a conexão e tente novamente.")
+
+if __name__ == "__main__":
+    main()
+
 
     else:
         print("Escolha inválida. Tente novamente.")
